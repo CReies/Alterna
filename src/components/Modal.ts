e6f49a5c3dda2042191c2f627ecfeb78ae66606f
CReies <ccreyesmolina@gmail.com>
2022-10-20 17:53:32 +0000

    added last sections

--- src/components/Modal.ts
+++
@@ -1,4 +0,0 @@
-export function setupModal(el: HTMLElement) {
-	el.innerHTML = `
-	`;
-}
--- src/util/functions.ts
+++
@@ -1,9 +0,0 @@
-import { d } from './consts';
-
-export const $ = (sel: string): HTMLElement => d.querySelector(sel)!;
-export const $$ = (sel: string): NodeListOf<HTMLElement> =>
-	d.querySelectorAll(sel)!;
-
-export const parsePrice = (price: number): string => {
-	return `$${new Intl.NumberFormat().format(price)}`;
-};
